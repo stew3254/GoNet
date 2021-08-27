48201b77329133dc39ac695c2143d1daf3b4e664
stew3254 <rtstewy@gmail.com>
2021-08-26 21:32:26 -0400

    Add all old files before changing

--- src/client.go
+++ src/client.go
@@ -1,118 +1,114 @@
 package main
 
 import (
-  "bufio"
-  "io"
-  "fmt"
-  "net"
-  //"github.com/gdamore/tcell"
-  "os"
+	"bufio"
+	"fmt"
+	"io"
+	"net"
 )
 
 type Client struct {
-  Con     net.Conn
-  Closed  chan bool
-  In      *os.File
-  Out     *os.File
+	Con    net.Conn
+	Closed chan bool
+	In     io.Reader
+	Out    io.Writer
 }
 
-//Implements io.Reader
+// Implements io.Reader
 func (c Client) Read(b []byte) (n int, err error) {
-  return c.Con.Read(b)
+	return c.Con.Read(b)
 }
 
-//Implements io.Writer
+// Implements io.Writer
 func (c Client) Write(b []byte) (n int, err error) {
-  return c.Con.Write(b)
+	return c.Con.Write(b)
 }
 
 func (c Client) Close() {
-  c.Con.Close()
+	c.Con.Close()
 }
 
-//Connection input
+// Connection input
 func (c Client) handleInput() {
-  reader := bufio.NewReader(c.In)
-  scanner := bufio.NewScanner(reader)
+	reader := bufio.NewReader(c.In)
+	scanner := bufio.NewScanner(reader)
 
-  input := make(chan bool)
-  sync := make(chan bool)
+	input := make(chan bool)
+	sync := make(chan bool)
 
-  //Look for input
-  go func() {
-    for {
-      input <- scanner.Scan()
-      <-sync
-    }
-  }()
+	// Look for input
+	go func() {
+		for {
+			input <- scanner.Scan()
+			<-sync
+		}
+	}()
 
-  //Check to see if the channel is still open
-  for {
-    select {
-    case closed := <-c.Closed:
-      if closed {
-        return
-      }
-    case <-input:
-      _, err := c.Write(append(scanner.Bytes(), byte('\n')))
-      if err != nil {
-        return
-      }
-      //Synchronize the states
-      sync <- true
-    }
-  }
-  return
+	// Check to see if the channel is still open
+	for {
+		select {
+		case closed := <-c.Closed:
+			if closed {
+				return
+			}
+		case <-input:
+			_, err := c.Write(append(scanner.Bytes(), byte('\n')))
+			if err != nil {
+				return
+			}
+			// Synchronize the states
+			sync <- true
+		}
+	}
+	return
 }
 
-//TODO add select to check for when this is closed
-//Connection output
+// TODO add select to check for when this is closed
+// Connection output
 func (c Client) handleOutput() {
-  buff := make([]byte, 4096)
+	buff := make([]byte, 4096)
 
-  input := make(chan int)
-  sync := make(chan bool)
+	input := make(chan int)
+	sync := make(chan bool)
 
-  //Look for input
-  go func() {
-    for {
-      n, err := c.Read(buff)
-      input <- n
-      if n == 0 {
-        //Catch EOF in case I handle this eventually
-        if err == io.EOF {
-          fmt.Fprintln(c.Out, "EOF")
-        }
-        break
-      }
-      //Sync the goroutines back up
-      <-sync
-    }
-  }()
+	// Look for input
+	go func() {
+		for {
+			n, err := c.Read(buff)
+			input <- n
+			if n == 0 {
+				// Catch EOF in case I handle this eventually
+				if err == io.EOF {
+					fmt.Fprintln(c.Out, "EOF")
+				}
+				break
+			}
+			// Sync the goroutines back up
+			<-sync
+		}
+	}()
 
-
-  //Loop until done
-  for {
-    select {
-    case n := <-input:
-      if n == 0 {
-        //Connection kill the connection or the connection already died
-        c.Closed <- true
-        c.Close()
-        return
-      }
-      fmt.Fprint(c.Out, string(buff))
-      //Zero the buffer out again
-      ssetn(buff, 0, n)
-      //Sync the goroutines
-      sync <- true
-    }
-  }
+	// Loop until done
+	for {
+		select {
+		case n := <-input:
+			if n == 0 {
+				// Connection kill the connection or the connection already died
+				c.Closed <- true
+				c.Close()
+				return
+			}
+			fmt.Fprint(c.Out, string(buff))
+			// Zero the buffer out again
+			ssetn(buff, 0, n)
+			// Sync the goroutines
+			sync <- true
+		}
+	}
 }
 
-//Handle connections
+// Handle connections
 func (c Client) Handle() {
-  go c.handleInput()
-  c.handleOutput()
+	go c.handleInput()
+	c.handleOutput()
 }
-
--- src/gn.go
+++ src/gn.go
@@ -1,146 +1,151 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "net"
-  //"github.com/gdamore/tcell"
-  "os"
-  "strconv"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
 )
 
-//Set the contents of a byte array to something
+// Set the contents of a byte array to something
 func sset(b []byte, e byte) {
-  for i := 0; i < len(b); i++ {
-    b[i] = e
-  }
+	for i := 0; i < len(b); i++ {
+		b[i] = e
+	}
 }
-//Connection output
 
-//Set the first n elements in byte array to something
+// Set the first n elements in byte array to something
 func ssetn(b []byte, e byte, n int) {
-  for i := 0; i < n; i++ {
-    b[i] = e
-  }
+	for i := 0; i < n; i++ {
+		b[i] = e
+	}
 }
 
-//Checks valid port
+// Checks valid port
 func isPort(p int) bool {
-  if p < 0 || p > 65535 {
-    return false
-  }
-  return true
+	if p < 0 || p > 65535 {
+		return false
+	}
+	return true
 }
 
 func main() {
-  //Get command line flags
-  portPtr := flag.Int("p", 0, "Set the source or listening port")
-  listenPtr := flag.Bool("l", false, "Listen for incoming connections")
-  udpPtr := flag.Bool("u", false, "Uses UDP instead of TCP")
-  verbosePtr := flag.Bool("v", false, "Verbose mode")
+	// Get command line flags
+	portPtr := flag.Int("p", 0, "Set the source or listening port")
+	listenPtr := flag.Bool("l", false, "Listen for incoming connections")
+	udpPtr := flag.Bool("u", false, "Uses UDP instead of TCP")
+	verbosePtr := flag.Bool("v", false, "Verbose mode")
 
-  flag.Parse()
+	flag.Parse()
 
-  //Flags
-  sport := *portPtr
-  listen := *listenPtr
-  udp := *udpPtr
-  verbose := *verbosePtr
+	// Flags
+	sport := *portPtr
+	listen := *listenPtr
+	udp := *udpPtr
+	verbose := *verbosePtr
 
-  //Client
-  var client Client
+	// Client
+	var client Client
 
-  //Initialize network vars
-  ip := ""
-  var dport int
-  var err error
+	// Initialize network vars
+	ip := ""
+	var dport int
+	var err error
 
-  //Get ip and port of thing to connect to
-  if !listen {
-    if flag.NArg() == 0 {
-      fmt.Fprintln(os.Stderr, "Missing destination and port")
-      return
-    } else if flag.NArg() == 1 {
-      fmt.Fprintln(os.Stderr, "Missing destination port")
-      return
-    } else {
-      //Look up host
-      addrs, err := net.LookupHost(flag.Arg(0))
-      if err != nil {
-        fmt.Fprintln(os.Stderr, err)
-        return
-      }
-      //Set ip and port
-      ip = addrs[0]
-      dport, err = strconv.Atoi(flag.Arg(1))
-      if err != nil {
-        fmt.Fprintln(os.Stderr, err)
-        return
-      } else if !isPort(dport) {
-        fmt.Fprintln(os.Stderr, err)
-      }
-    }
-    //Start UDP or TCP connection
-    if udp {
-      //Check if source port is set
-      if isPort(sport) {
-        laddr, _ := net.ResolveUDPAddr("udp", ":" + strconv.Itoa(sport))
-        raddr := &net.UDPAddr {IP: net.ParseIP(ip), Port: dport, Zone: ""}
-        client.Con, err = net.DialUDP("udp", laddr, raddr)
-      } else {
-        client.Con, err = net.Dial("udp", net.JoinHostPort(ip, strconv.Itoa(dport)))
-      }
-    } else {
-      client.Con, err = net.Dial("tcp", net.JoinHostPort(ip, strconv.Itoa(dport)))
-    }
-    //Check to see if the connection is valid
-    if err != nil {
-      fmt.Fprintln(os.Stderr, "Failed to connect")
-      return
-    }
-  } else {
-    //Make sure port is valid
-    if !isPort(sport) {
-      fmt.Fprintln(os.Stderr, "Port number is invalid")
-      return
-    }
+	// Get ip and port of thing to connect to
+	if !listen {
+		if flag.NArg() == 0 {
+			fmt.Fprintln(os.Stderr, "Missing destination and port")
+			return
+		} else if flag.NArg() == 1 {
+			fmt.Fprintln(os.Stderr, "Missing destination port")
+			return
+		} else {
+			// Look up host
+			addrs, err := net.LookupHost(flag.Arg(0))
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				return
+			}
+			// Set ip and port
+			ip = addrs[0]
+			dport, err = strconv.Atoi(flag.Arg(1))
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				return
+			} else if !isPort(dport) {
+				fmt.Fprintln(os.Stderr, err)
+			}
+		}
+		// Start UDP or TCP connection
+		if udp {
+			// Check if source port is set
+			if isPort(sport) {
+				laddr, _ := net.ResolveUDPAddr("udp", ":"+strconv.Itoa(sport))
+				raddr := &net.UDPAddr{IP: net.ParseIP(ip), Port: dport, Zone: ""}
+				client.Con, err = net.DialUDP("udp", laddr, raddr)
+			} else {
+				client.Con, err = net.Dial("udp", net.JoinHostPort(ip, strconv.Itoa(dport)))
+			}
+		} else {
+			client.Con, err = net.Dial("tcp", net.JoinHostPort(ip, strconv.Itoa(dport)))
+		}
+		// Check to see if the connection is valid
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to connect")
+			return
+		}
+	} else {
+		// Make sure port is valid
+		if !isPort(sport) {
+			fmt.Fprintln(os.Stderr, "Port number is invalid")
+			return
+		}
 
-    //Check args
-    if flag.NArg() > 0 {
-      ip = flag.Arg(0)
-    }
+		// Check args
+		if flag.NArg() > 0 {
+			ip = flag.Arg(0)
+		}
 
-    //UDP vs TCP listener
-    var l net.Listener
-    if udp {
-      //TODO fix this
-      addr, err := net.ResolveUDPAddr("udp", net.JoinHostPort(ip, strconv.Itoa(sport)))
-      if err != nil {
-        fmt.Fprintln(os.Stderr, err)
-        return
-      }
-      client.Con, _ = net.ListenUDP("udp", addr)
-    } else {
-      l, err = net.Listen("tcp", net.JoinHostPort(ip, strconv.Itoa(sport)))
-      if err != nil {
-        fmt.Fprintln(os.Stderr, err)
-        return
-      }
-      defer l.Close()
+		// UDP vs TCP listener
+		var l net.Listener
+		if udp {
+			// TODO fix this
+			laddr, err := net.ResolveUDPAddr("udp", net.JoinHostPort(ip, strconv.Itoa(sport)))
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				return
+			}
+			client.Con, _ = net.ListenUDP("udp", laddr)
+			// Wait until a client sends something to know how to connect back to them
+			b := make([]byte, 4096)
+			_, raddr, _ := client.Con.(*net.UDPConn).ReadFromUDP(b)
+			// TODO Remember to fix this when client.In/Out matters more
+			fmt.Print(string(b))
+			client.Con.Close()
+			client.Con, err = net.DialUDP("udp", laddr, raddr)
+		} else {
+			l, err = net.Listen("tcp", net.JoinHostPort(ip, strconv.Itoa(sport)))
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				return
+			}
+			defer l.Close()
 
-      //Only allow one connection for now
-      client.Con, err = l.Accept()
-      if verbose {
-        fmt.Println("Connection from " + client.Con.RemoteAddr().String() + " succeeded!")
-      }
-      if err != nil {
-        fmt.Fprintln(os.Stderr, err)
-      }
-    }
-  }
+			// Only allow one connection for now
+			client.Con, err = l.Accept()
+			if verbose {
+				fmt.Println("Connection from " + client.Con.RemoteAddr().String() + " succeeded!")
+			}
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			}
+		}
+	}
 
-  client.Closed = make(chan bool)
-  client.In = os.Stdin
-  client.Out = os.Stdout
-  client.Handle()
+	client.Closed = make(chan bool)
+	client.In = os.Stdin
+	client.Out = os.Stdout
+	client.Handle()
 }
